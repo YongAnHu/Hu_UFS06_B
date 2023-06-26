85be9750165b6a6c9a069e11d4ccf2cb21725587
YongAnHu <huyongan.yah@gmail.com>
2023-06-26 09:00:25 +0200

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * Traccia: B
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
