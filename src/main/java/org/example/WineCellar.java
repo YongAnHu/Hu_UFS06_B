f0455f77432c81118e131b1e2d36cebc2ab54e2a
YongAnHu <huyongan.yah@gmail.com>
2023-06-26 10:14:24 +0200

    Removed unicode characters from Wine names Modified sorted_by_price to be from most expensive to least expensive

--- src/main/java/org/example/WineCellar.java
+++ src/main/java/org/example/WineCellar.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,15 +9,18 @@ import java.util.List;
 public class WineCellar {
     private static WineCellar INSTANCE;
     private List<Wine> wineList = new ArrayList<>();
+    private Gson gson = new GsonBuilder()
+            .setPrettyPrinting()
+            .create();
 
     private WineCellar() {
         // IF type is true -> type = white
         // IF type is false -> type = red
         wineList.add(new Wine(2, "Pignoli Radikon Radikon 2009", 133,  false));
-        wineList.add(new Wine(1, "Dom Perignon Vintage Moet & Chandon 2008", 225.94, true));
+        wineList.add(new Wine(1, "Dom Perignon Vintage Moet and Chandon 2008", 225.94, true));
         wineList.add(new Wine(3, "Pinot Nero Elena Walch Elena Walch 2018", 43, false));
         wineList.add(new Wine(4, "Penfolds Yattarna Chardonnay 2015", 199.65, true));
-        wineList.add(new Wine(5, "Conterno Giacomo Barbera D'alba Doc Vigna Francia 2019", 71.10, false));
+        wineList.add(new Wine(5, "Conterno Giacomo Barbera D alba Doc Vigna Francia 2019", 71.10, false));
     }
 
     public static WineCellar getInstance() {
@@ -28,7 +32,6 @@ public class WineCellar {
     }
 
     public String getWine(String type) {
-        Gson gson = new Gson();
         ArrayList<Wine> save = new ArrayList<>();
 
         for (Wine wine: wineList) {
@@ -42,8 +45,6 @@ public class WineCellar {
     }
 
     public String getSortedName() {
-        Gson gson = new Gson();
-
         List<Wine> listCopy = new ArrayList<>();
         listCopy.addAll(wineList);
 
@@ -55,16 +56,14 @@ public class WineCellar {
     }
 
     public String getSortedPrice() {
-        Gson gson = new Gson();
-
         List<Wine> listCopy = new ArrayList<>();
         listCopy.addAll(wineList);
 
         listCopy.sort((o1, o2) -> {
             if (o1.getPrice()>o2.getPrice())
-                return 1;
-            if (o1.getPrice()<o2.getPrice())
                 return -1;
+            if (o1.getPrice()<o2.getPrice())
+                return 1;
             return 0;
         });
 
